e42e7e984d5feaf166e7a1881a5875b3c56554cd
OlgaZar <116227489+OlgaZar@users.noreply.github.com>
2023-03-26 17:19:10 +0300

    changes in class PersonName

--- src/main/java/javadevhw1gradle/PersonName.java
+++ src/main/java/javadevhw1gradle/PersonName.java
@@ -1,8 +1,10 @@
 package javadevhw1gradle;
 
+import java.util.Objects;
+
 public class PersonName {
-    String name;
-    String lastName;
+    private String name;
+    private String lastName;
 
     public PersonName(String name, String lastName) {
         this.name = name;
@@ -23,5 +25,26 @@ public class PersonName {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonName that = (PersonName) o;
+        return Objects.equals(name, that.name) && Objects.equals(lastName, that.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "PersonName{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
     }
 }
